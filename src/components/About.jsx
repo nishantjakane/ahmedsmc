75cec6dc4e5e0cc2a7b57c8322f74d2c4686a644
Nishant212121 <savitarjakane@gmail.com>
2023-08-04 19:37:57 +0530

    Img change

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -22,7 +22,7 @@ function About(){
             </p>
         </div>
                     <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
-                        <img src="./assets/aboutImg" alt="mockup"></img>
+                        <img src="./assets/aboutImg.png" alt="mockup"></img>
                     </div>                
                 </div>
             </section>
