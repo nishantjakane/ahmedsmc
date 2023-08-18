27dad50fee911b172fa1577ea3a25bc4970705bb
Nishant212121 <savitarjakane@gmail.com>
2023-08-18 18:57:30 +0530

    a

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -23,7 +23,7 @@ function About(){
             </p>
         </div>
                 <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
-                <img src="./assets/aboutImg.png" alt="mockup" className="aboutImg"></img>
+                <img src="./assets/aboutImg2.png" alt="mockup" className="aboutImg"></img>
             </div>                
             </div>
             </section>
